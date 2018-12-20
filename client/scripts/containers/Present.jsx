74c5a74e641187a3b46a98817efe9901506d2fc2
Aleksandar Stojanovikj <ace92.work@gmail.com>
2018-12-20 09:31:48 +0100

    fix login register and profile page

--- client/scripts/containers/Present.jsx
+++ client/scripts/containers/Present.jsx
@@ -5,9 +5,13 @@ import {Row,Col,Input,Icon,Button,Card,CardTitle,Modal} from 'react-materialize'
 //import components and containers
 import ModalForDogsCards from './ModalForDogsCards.jsx';
 //import assets
+import akita from '../../styles/assets/img/akita.jpg';
 import beagle from '../../styles/assets/img/beagle.jpg';
 import samojed from '../../styles/assets/img/samojed.jpeg';
-
+import haska from '../../styles/assets/img/haska.jpg';
+import dognew from '../../styles/assets/img/dog-new.jpg';
+import dognew2 from '../../styles/assets/img/dog-new2.jpg';
+import dognew3 from '../../styles/assets/img/dog-new3.jpg';
 class Pop extends React.Component {
     render(){
         return(
@@ -30,25 +34,37 @@ export default class Present extends React.Component {
                     <Col s={12}>
                         <Col s={12} m={6} l={4} >
                             <Card horizontal responsive header={<CardTitle  image={beagle}></CardTitle>} >
-                                <p>Gala Stratrova</p>
+                                <p>Gala ID:Greta</p>
                                 <Pop/>
                             </Card>
                         </Col>
                         <Col s={12} m={6} l={4}>
                             <Card horizontal header={<CardTitle image={samojed}></CardTitle>}  >
-                                <p>Aron Stojanovikj</p>
+                                <p>Aron ID:Brazil</p>
                                 <Pop/>
                             </Card>
                         </Col>
                         <Col s={12} m={6} l={4}>
-                            <Card horizontal header={<CardTitle image={beagle}></CardTitle>}  >
-                                <p>Gala Stratrova</p>
+                            <Card horizontal header={<CardTitle image={haska}></CardTitle>}  >
+                                <p>Aleks ID:Ake</p>
                                 <Pop/>
                             </Card>
                         </Col>
                         <Col s={12} m={6} l={4}>
-                            <Card horizontal header={<CardTitle image={samojed}></CardTitle>}  >
-                                <p>Aron Stojanovikj</p>
+                            <Card horizontal header={<CardTitle image={dognew}></CardTitle>}  >
+                                <p>Ringo ID:Baron</p>
+                                <Pop/>
+                            </Card>
+                        </Col>
+                        <Col s={12} m={6} l={4}>
+                            <Card horizontal header={<CardTitle image={akita}></CardTitle>}  >
+                                <p>Hektor ID:Velko</p>
+                                <Pop/>
+                            </Card>
+                        </Col>
+                        <Col s={12} m={6} l={4}>
+                            <Card horizontal header={<CardTitle image={dognew3}></CardTitle>}  >
+                                <p>Sarko ID:Bimbo</p>
                                 <Pop/>
                             </Card>
                         </Col>
