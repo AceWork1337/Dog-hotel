806a619157cb9774e37b339c4cdc47e4a9f0ee7c
Aleksandar <ace92.work@gmail.com>
2019-03-11 14:57:11 +0100

    Fix on mouse move

--- client/scripts/containers/HomeParalax.jsx
+++ client/scripts/containers/HomeParalax.jsx
@@ -1,6 +1,6 @@
 //import npm package
 import React from 'react';
-import {Carousel,Grid,Row,Col,Image} from 'react-bootstrap';
+import { Carousel, Grid, Row, Col, Image } from 'react-bootstrap';
 import { Slider, Slide } from 'react-materialize';
 import { Parallax } from 'react-scroll-parallax';
 import ParallaxMousemove from 'react-parallax-mousemove'
@@ -25,209 +25,173 @@ import SamoyedWhite from '../../styles/assets/img/SamoyedWhite.jpg';
 import pethealth from '../../styles/assets/img/pet-health.jpg';
 import pethotel from '../../styles/assets/img/pet-hotel.jpg';
 import PetGrooming from '../../styles/assets/img/Pet-Grooming.jpg';
-import HouseImg from '../../styles/assets/img/cipo-houseslider.png';
+import HouseImg from '../../styles/assets/img/cipo-houseslider.jpg';
 import HouseImgW from '../../styles/assets/img/cipo-housesliderW.png';
-import DogImg from '../../styles/assets/img/cipo-dog.png';
-import DogImg1 from '../../styles/assets/img/cipo-dog-1.png';
+import DogImg from '../../styles/assets/img/Dog2.png';
+import DogImg1 from '../../styles/assets/img/GoldPet.png';
 import DogImgHead from '../../styles/assets/img/cipo-houseslider-head.png';
 
 export default class HomeParalax extends React.Component {
 
-    render() {
-  
+  render() {
+
     const styleImgHouse = {
-        // marginLeft : '248em',
-        width:'105%',
-        height:'auto',
-        opacity: '0.16',
-        margin: '0px -600px'
+      // marginLeft : '248em',
+      width: '105%',
+      height: 'auto',
+      opacity: '0.16',
+      margin: '0px -600px'
     }
     const styleImg1 = {
-        // marginLeft : '-310px',
-        marginTop: '780px',
-        width:'100%',
-        
+      // marginLeft : '-310px',
+      marginTop: '780px',
+      width: '100%',
+
     }
     const styleImg2 = {
-        marginLeft : '730px',
-        width:'45%',
-        // height:'200px',
-        
+      marginLeft: '730px',
+      width: '45%',
+      // height:'200px',
+
     }
     const style = {
-        outter: {
-          backgroundImage: `url(${HouseImg})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          width:'100%',
-          position: 'relative',
-          overflow: 'hidden'
-        },
-        inner: {
-          width:700,
-          height:500,
-          position: 'absolute',
-          margin:'auto',
-          top:0,
-          left:0,
-          right:0,
-          bottom:0,
-          textAlign:'center'
-        },
-        header: {
-          fontFamily: 'Open Sans Condensed',
-          textTransform: 'uppercase',
-          color: 'white',
-          textShadow: '0px 0px 2px #43AABA',
-          fontSize: '50px',
-          marginTop: '35px',
-          fontWeight: 600
-        },
-        paragraph: {
-          fontFamily: 'Roboto',
-          fontSize: '20px',
-          color: 'white',
-          letterSpacing: '0.62px',
-          lineHeight: '30px',
-          fontWeight: 300
-        },
-        button: {
-          fontFamily: 'Roboto',
-          borderRadius: '100px',
-          background: '#247B8A',
-          textDecoration: 'none',
-          color: 'white',
-          textAlign: 'center',
-          padding: '20px',
-          marginTop: '40px',
-          fontWeight: 100,
-          display: 'block'
-        },
-        infoLayerStyle: {
-          position: 'relative',
-          marginTop: '-62%',
-          marginLeft: '-220%',
-        },
-        infoLayerStyle2: {
-            position: 'relative',
-            marginTop: '-40%',
-            marginLeft: '50rem',
-          },
-        bgLayerStyle: {
-          position: 'relative',
-          marginTop: '60%',
-          marginLeft:'-90%',
-          height: '100%',
-          transform: 'translate(-15%, 17%)',
-        },
-        bgLayerStyle2: {
-            position: 'relative',
-            marginTop: '-13%',
-            marginLeft:'15%',
-            height: '100%',
-            width:'100%',
-            transform: 'translate(-15%, 17%)',
-            opacity: '0.3',
-          }
+      outter: {
+        // backgroundImage: `url(${HouseImg})`,
+        // backgroundPosition: 'center',
+        // backgroundSize: 'cover',
+        width: '100%',
+        // height:'1440px',
+        position: 'relative',
+        overflow: 'hidden'
+      },
+      inner: {
+        width: 700,
+        height: 500,
+        position: 'absolute',
+        margin: 'auto',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        textAlign: 'center'
+      },
+      // header: {
+      //   fontFamily: 'Open Sans Condensed',
+      //   textTransform: 'uppercase',
+      //   color: 'white',
+      //   textShadow: '0px 0px 2px #43AABA',
+      //   fontSize: '50px',
+      //   marginTop: '35px',
+      //   fontWeight: 600
+      // },
+      // paragraph: {
+      //   fontFamily: 'Roboto',
+      //   fontSize: '20px',
+      //   color: 'white',
+      //   letterSpacing: '0.62px',
+      //   lineHeight: '30px',
+      //   fontWeight: 300
+      // },
+      // button: {
+      //   fontFamily: 'Roboto',
+      //   borderRadius: '100px',
+      //   background: '#247B8A',
+      //   textDecoration: 'none',
+      //   color: 'white',
+      //   textAlign: 'center',
+      //   padding: '20px',
+      //   marginTop: '40px',
+      //   fontWeight: 100,
+      //   display: 'block'
+      // },
+      infoLayerStyle: {
+        position: 'relative',
+        // marginTop: '-62%',
+        // marginLeft: '-220%',
+        width:'15%',
+        marginTop:'-50%',
+        marginLeft:'-10%',
+      },
+      infoLayerStyle2: {
+        position: 'relative',
+        // marginTop: '-40%',
+        // marginLeft: '50rem',
+        marginTop: '5%',
+        width:'20%',
+        marginLeft:'70%',
+      },
+      bgLayerStyle: {
+        position: 'relative',
+        // marginTop: '60%',
+        // marginLeft: '-90%',
+        height: '100%',
+        transform: 'translate(-15%, 17%)',
+        width:'55%',
+        marginLeft:'30%',
+        marginTop:'-30%',
+      },
+      bgLayerStyle2: {
+        position: 'relative',
+        // marginTop: '-13%',
+        // marginLeft: '15%',
+        height: '100%',
+        width: '100%',
+        // transform: 'translate(-15%, 17%)',
+        // opacity: '0.3',
       }
-        return (
-           <div className="home-page-paralax" style={style}>
-           {/* <img id="ss" src={HouseImg}></img> */}
-                {/* <Parallax
-                    className="custom-class"
-                    offsetXMax={-30}
-                    offsetXMin={30}
-                    slowerScrollRate
-                    tag="figure"
-                >
-                    <Image src={HouseImg} style={styleImgHouse}/>
-                </Parallax> */}
-                {/* <Parallax
-                    className="custom-class"
-                    offsetYMax={200}
-                    offsetYMin={800}
-                    slowerScrollRate
-                    tag="figure"
-                >
-                    <Image src={DogImgHead} />
-                </Parallax> */}
-                {/* <Parallax
-                
-                    className="custom-class"
-                    offsetXMax={-50}
-                    offsetXMin={50}
-                    slowerScrollRate
-                    tag="figure"
-                >
-                    <Image src={DogImg1} style={styleImg1}/>
-                </Parallax> */}
-                {/* <Parallax
-                    className="custom-class"
-                    offsetYMax={200}
-                    offsetYMin={-200}
-                    slowerScrollRate
-                    tag="figure"
-                >
-                    <Image src={DogImg} style={styleImg2}/>
-                </Parallax> */}
-                <ParallaxMousemove containerStyle={style.outter} fullHeight={true}>
-          
+    }
+    return (
+      <Row>
+      <div className="home-page-paralax" style={style}>
+
+        <ParallaxMousemove containerStyle={style.outter} fullHeight={true}>
+
           <ParallaxMousemove.Layer layerStyle={style.bgLayerStyle2} config={{
-              xFactor: 0.05,
-              yFactor: 0,
-              springSettings: {
-                stiffness: 50,
-                damping: 30
-              }
-            }}>
-            <img src={HouseImgW} alt="Parallax Layer"></img>
+            xFactor: 0,
+            yFactor: 0,
+            springSettings: {
+              stiffness: 50,
+              damping: 30
+            }
+          }}>
+          <img src={HouseImg} alt="Parallax Layer"></img>
           </ParallaxMousemove.Layer>
-          <div style={style.inner}>
-          <ParallaxMousemove.Layer  layerStyle={style.bgLayerStyle} config={{
-              xFactor: 0.02,
-              yFactor: 0,
+          {/* <div style={style.inner}> */}
+            <ParallaxMousemove.Layer layerStyle={style.bgLayerStyle} config={{
+              xFactor: 0,
+              yFactor: -0.5,
               springSettings: {
                 stiffness: 50,
                 damping: 50
               }
             }}>
-            <img src={DogImgHead}  alt="Parallax Layer"></img>
-          </ParallaxMousemove.Layer>
-            {/* <ParallaxMousemove.Layer layerStyle={style.infoLayerStyle} config={{
-                xFactor: 0.2,
-                yFactor: 0,
-                springSettings: {
-                  stiffness: 50,
-                  damping: 30
-                }
-              }}>
-              <img src={DogImg1} alt="Parallax Layer"></img>
-            </ParallaxMousemove.Layer> */}
-            
+              <img src={DogImgHead} alt="Parallax Layer"></img>
+            </ParallaxMousemove.Layer>
+
             <ParallaxMousemove.Layer layerStyle={style.infoLayerStyle} config={{
-                xFactor: 0.05,
-                yFactor: 0,
-                springSettings: {
-                  stiffness: 50,
-                  damping: 30
-                }
-              }}>
+              xFactor: -0.6,
+              yFactor: 0,
+              springSettings: {
+                stiffness: 80,
+                damping: 20
+              }
+            }}>
               <img src={DogImg1} alt="Parallax Layer"></img>
             </ParallaxMousemove.Layer>
             <ParallaxMousemove.Layer layerStyle={style.infoLayerStyle2} config={{
-                xFactor: 0,
-                yFactor: -0.1,
-                springSettings: {
-                  stiffness: 50,
-                  damping: 30
-                }
-              }}>
+              xFactor: 0,
+              yFactor: -1.2,
+              springSettings: {
+                stiffness: 80,
+                damping: 20
+              }
+            }}>
               <img src={DogImg} alt="Parallax Layer"></img>
             </ParallaxMousemove.Layer>
-          </div>
+          {/* </div> */}
         </ParallaxMousemove>
-            </div> 
-           
-        );
-    }
+      </div>
+      </Row>
+    );
+  }
 }
